31cc08b68f1e90b3f2369374e5b9baa50ccdbb0d
Shawn <nhatquangta2911@gmail.com>
2021-05-14 16:21:26 +0700

    Test heroku

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -5,3 +5,7 @@ export const homeRouter: Router = express.Router();
 homeRouter.get('/', (req: Request, res: Response) => {
    res.send('Home Page');
 })
+
+homeRouter.get('/test', (req: Request, res: Response) => {
+   res.send('Test');
+})
