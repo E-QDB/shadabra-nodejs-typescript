44a7d97aad3e497b6cdd1a03bc92c8629a081049
Shawn <nhatquangta2911@gmail.com>
2021-05-16 17:18:19 +0700

    Completed CRUD for teachers

--- src/routes/teacher.ts
+++ src/routes/teacher.ts
@@ -1,7 +1,11 @@
-import express, {Router} from 'express';
-import {getTeachers, addTeacher} from '../controllers';
+import {Router} from 'express';
+import {getTeachers, addTeacher, updateTeacher, getTeacherById, deleteTeacher} from '../controllers';
+import { validateObjectID } from '../middlewares';
 
 export const teacherRouter = Router();
 
 teacherRouter.get('/', getTeachers);
 teacherRouter.post('/', addTeacher);
+teacherRouter.get('/:id', validateObjectID, getTeacherById);
+teacherRouter.patch('/:id', validateObjectID, updateTeacher);
+teacherRouter.delete('/:id', validateObjectID, deleteTeacher)
